98b045e3ebcf61a99e141ae6d2b412c3a2824d9d
samjyi95 <samjyi95@gmail.com>
2020-05-15 09:49:07 -0700

    implemented login route; created profile controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,74 @@
 require('dotenv').config()
 let db = require('../models')
 let router = require('express').Router()
+let jwt = require('jsonwebtoken')
 
 // POST /auth/login (find and validate user; send token)
 router.post('/login', (req, res) => {
   console.log(req.body)
-  res.send('STUB POST /auth/login')
+
+  db.User.findOne({ email: req.body.email})
+  .then(user => {
+    //check whether the user exists
+    if (!user) {
+      return res.status(404).send({ message: 'User was not found '})
+    }
+
+    //They exist - but make sure they have the correct passwor
+    if (!user.validPasswords(req.body.password)) {
+      //incorrect pasword, send error 
+      return res.status(401).send({ message: 'invalid credentials '})
+    }
+
+    //We have a good user - make them a token 
+    let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+      expiresIn: 60*60*8 //8hours in secs
+    }) 
+    res.send({ token })
+  })
+  .catch(err => {
+    console.log('Error in POST auth/login', err)
+    res.status(503).send({ message: 'Server-side or DB error'})
+  })
 })
 
 // POST to /auth/signup (create user; generate token)
 router.post('/signup', (req, res) => {
   console.log(req.body)
-  res.send('STUB POST /auth/signup')
+  //look up the user by email to make sure they're new 
+  db.User.findOne({email: req.body.email })
+  .then(user => {
+    //if the user exists already, do not let them create another account 
+    if (user) {
+      //NO no1 sign up instead
+      return res.status(416).send({ message: 'Email already in use' })
+    }
+
+    //we know the user is legitamely a new user: Create them!!
+      db.User.create(req.body)
+      .then(newUser => {
+        //Yay1 things worked and the user exists! Now Create a new token for the new user
+        let token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET, {
+          expiresIn: 120//60 * 60 * 8 // 8 hours in seconds
+        }) 
+
+        res.send({ token })
+      })
+      .catch(innerErr => {
+        console.log('Error creating user', innerErr)
+        if (innerErr.name === 'ValidationError') {
+          res.status(412).send({message: `Validation Error! ${innerErr.message}.` })
+        }
+        else {
+          res.status(500).send({ message: 'Error creating user'})
+        }
+      })
+  })
+  .catch(err => {
+    console.log('ERROR IN POST /auth/signup', err)
+    res.status(503).send({ message: 'Database or server error' })
+  })
 })
 
-// NOTE: User should be logged in to access this route
-router.get('/profile', (req, res) => {
-  // The user is logged in, so req.user should have data!
-  // TODO: Anything you want here!
-
-  // NOTE: This is the user data from the time the token was issued
-  // WARNING: If you update the user info those changes will not be reflected here
-  // To avoid this, reissue a token when you update user data
-  res.send({ message: 'Secret message for logged in people ONLY!' })
-})
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -1,5 +1,59 @@
+let bcrypt = require('bcryptjs')
 let mongoose = require('mongoose')
 
 // TODO: Create user schema
+let userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    pic: String,
+    admin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+//TODO: Hash the passwords
+userSchema.pre('save', function(done) {
+    //Make syre uts bew, as opposed to modified
+    if ( this.isNew ) {
+        this.password = bcrypt.hashSync(this.password, 12)
+    }
+    //Tell it we're okay to move on 
+    done()
+})
+
+// Make a JSON reprensentation of the user (for sending on the JWT payload)
+// We're deleing password bc we dont want to pass the token back to the user their own pw wjen the token can be seen by everyone 
+userSchema.set('toJSON', {
+    transform: (doc, user) => {
+        delete user.password
+        delete user.lastname 
+        delete user.__v
+        return user
+    }
+})
+
+//TODO: Make a function that compares the passwords
+// think of this as one specific user and one specific user object data, this is basically referring to the user's hashed password 
+userSchema.methods.validPasswords = function (typedPassword) {
+//typedpassword: PLain text, just typed in by user
+//this.password: Existing hashed password
+    return bcrypt.compareSync(typedPassword, this.password)
+} 
 
 // TODO: Export user model
+module.exports = mongoose.model('User', userSchema)
